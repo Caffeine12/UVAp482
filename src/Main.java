9a770a04dd9ad6bb8c42fe456edc29773fb6697c
Jubair <jubairku12@gmail.com>
2024-01-05 02:48:19 +0200

    Presentation Error-Need to find out what is wrong

--- src/Main.java
+++ src/Main.java
@@ -4,18 +4,16 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
         int cases=  sc.nextInt();
+        String blank = sc.nextLine();
 
         while(cases>0)
-        {
-            String blank = sc.nextLine();
+            {
+            
             String blankTwo = sc.nextLine();
-
             String order = sc.nextLine();
             String numbers = sc.nextLine();
-
             String[] orderArr = order.split(" ");
             String[] numbersArr = numbers.split(" ");
-
             for(int j=1; j<=orderArr.length;j++)
             {
                 for(int k=0; k<orderArr.length;k++)
@@ -26,10 +24,10 @@ public class Main {
                         break;
                     }
                 }
-
-            }          
-        
+            } 
+            System.out.println();                     
             cases--;
         }
+        sc.close();
     }
 }
